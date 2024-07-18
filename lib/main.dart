2219d1230917a916951618ec01fc04c93081cb72
HashSahu <piyushsahu436@gmail.com>
2024-07-18 10:41:51 +0530

    dartmain

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
-
 import 'package:flutter/material.dart';
-import 'package:solocaht/login_page.dart';
+import 'package:solocaht/models/login_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
+  runApp(const MyApp());
+}
 
-  runApp(g
-    MaterialApp(
-      home: LoginPage(
-        onTap: () {
-          // Add your onTap action here
-          print("Register now tapped");
-        },
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'chatappsolo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-    ),
-  );
+      debugShowCheckedModeBanner: false,
+      home: login(),
+    );
+  }
 }
